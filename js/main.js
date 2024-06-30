07cb6c5bbac07d2279231e403e98fd651a4f6b21
Omair Mohammed <126175145+omair-660@users.noreply.github.com>
2024-06-30 18:02:35 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ import { GameList } from './GameList.js';
 document.addEventListener("DOMContentLoaded", () => {
     new GameList();
 });
+
+let spans = document.querySelector(".spans");
+let span = document.querySelectorAll(".spans span");
+
+spans.addEventListener("click",function(){
+    for (let i = 0; i < span.length; i++) {
+       span[i].classList.toggle("active")
+        
+    }
+})
